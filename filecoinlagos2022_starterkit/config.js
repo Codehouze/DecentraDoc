e279da598306c0fb2d0b1a533614b75b5712b75f
KingzRex <toochukwukingz6@gmail.com>
2022-04-01 16:54:01 +0000

    base_setup

---
+++ filecoinlagos2022_starterkit/config.js
@@ -0,0 +1,3 @@
+export const nftAddress = '0x0000000000000000......'; //your deployed contract addresss
+export const storageApiToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDFmNDU0MkE1ZjVFMGRFMjA3OUUzODhmYTk1NTBhMWJENkNDRDFlMDMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2MzcyNjc0NDI2NDgsIm5hbWUiOiJBZ29yYSJ9.5B3NBLWaG4bxoO1QR_rrU-eYnqYq9jDf_VcGYt7nmOQ';
