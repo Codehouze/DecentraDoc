e1524e918a1450cd8bb793879a5bf12af716e46f
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2022-04-02 13:12:12 +0100

    WIP

--- filecoinlagos2022_starterkit/src/App.js
+++ filecoinlagos2022_starterkit/src/App.js
@@ -1,7 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const { ethereum } = window;
+
+  const getEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const decentraDocContract = new ethers.Contract(
+      contractAddress,
+      contractAbi,
+      signer
+    );
+
+    return decentraDocContract;
+  };
+
+  const checkWalletConnection = async () => {
+    try {
+      if (!ethereum) return alert("Please install metamask");
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length) {
+        setCurrentAccount(accounts[0]);
+      } else {
+        console.log("No accounts found");
+      }
+
+      console.log(accounts);
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No ethereum object");
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("Please install metamask");
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No ethereum object");
+    }
+  };
+
+  useEffect(() => {
+    checkWalletConnection();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ filecoinlagos2022_starterkit/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./DecentraDoc.json";
+
+export const contractAbi = abi.abi;
+export const contractAddress = "0xDee7504c6EC38AEE20435448dB1eB573bF20Ccf8";
